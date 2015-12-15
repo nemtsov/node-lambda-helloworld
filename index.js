ec1df65db0fde613d352422d179b34c175094b9e
Yuriy Nemtsov <nemtsov@gmail.com>
2015-12-15 11:38:30 -0500

    remove sass dep

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-var sass = require('node-sass');
-
 exports.handler = function (event, context) {
-  var style = sass.renderSync({data: 'a { color: blue; }'}).css.toString();
-  context.succeed('<a style="' + style + '">Hello world!</a>');
+  context.succeed('<a>Hello world!</a>');
 };
